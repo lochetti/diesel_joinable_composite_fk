add412c56e3deee60c4c7bd9dc4c9d16789baf81
Renato Lochetti <renato.lochetti@gmail.com>
2022-12-14 14:51:20 +0000

    Adding explanation about the problem in the schema.rs file

--- src/schema.rs
+++ src/schema.rs
@@ -23,8 +23,8 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(transaction_one -> payment_card (card_code));
-diesel::joinable!(transaction_two -> payment_card (payment_card_id));
+diesel::joinable!(transaction_one -> payment_card (card_code)); // This joinable is wrong and, in that case, definitely not safe. 
+diesel::joinable!(transaction_two -> payment_card (payment_card_id)); // This joinable is wrong because the FK is composite and might not be safe.
 
 diesel::allow_tables_to_appear_in_same_query!(
     payment_card,
